d9451b28cc827b6b4f23b4f324cbf3be41712e98
funes16 <juanhernandezfunes@gmail.com>
2026-07-26 23:58:55 -0600

    se mejora script mysql para crear la tabla con indices

--- doc/Crear_tabla.sql
+++ doc/Crear_tabla.sql
@@ -1,10 +1,12 @@
 -- Estructura de tabla para la tabla `books`
 CREATE TABLE `books` (
   `ID` int(11) NOT NULL,
-  `CODE` varchar(255) NOT NULL,
+  `CODE` varchar(3) NOT NULL,
   `NAME` varchar(255) NOT NULL,
   `AUTHOR` varchar(255) NOT NULL,
-  `GENRE` varchar(255) NOT NULL
+  `GENRE` varchar(255) NOT NULL,
+  `PUBLICATION_DATE` date NOT NULL,
+  `CREATE_DATE` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Indices de la tabla `books`
@@ -17,4 +19,3 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT de la tabla `books`
 ALTER TABLE `books`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
